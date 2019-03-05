5490a70cab19bf2256b21bd1ca869996d3a421f0
byteofsoren <r00tr4t@gmail.com>
2019-03-05 11:18:39 +0100

    Changed classify
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- classify.py
+++ classify.py
@@ -7,7 +7,8 @@ from split import splitData
 from plot import gen_frequesyresponse, afrequesy_plot
 
 def prune_f(signal:list, cut:list):
-    ret = np.array(signal[1])
+    ret = np.array(signal)
+    #ret = np.array(signal[1])
     assert(len(ret.shape) == 1)
     for x in np.arange(ret.size):
         if not (cut[0] < x and x < cut[1]):
@@ -26,9 +27,27 @@ if __name__ == '__main__':
     cutwalking = [17,21]
     cutrunning = [24,28]
     data = splitData()
-    print(test_signal(data['run'][0], cutrunning))
-    print(test_signal(data['run'][0], cutwalking))
-    print(test_signal(data['walk'][0], cutrunning))
-    print(test_signal(data['walk'][0], cutwalking))
+    # print(test_signal(data['run'][0], cutrunning))
+    # print(test_signal(data['run'][0], cutwalking))
+    # print(test_signal(data['walk'][0], cutrunning))
+    # print(test_signal(data['walk'][0], cutwalking))
+
+    for key in data.keys():
+        if key == 'run':
+            cut = cutrunning
+            plt.subplot(2,1,1)
+        else:
+            cut = cutwalking
+            plt.subplot(2,1,2)
+        plt.xlim([15,30])
+        plt.title("{} filerd".format(key))
+        for signal in data[key]:
+            fsignal = gen_frequesyresponse(signal)
+            psignal = prune_f(fsignal['magnitude'], cut)
+            plt.plot(psignal)
+    plt.show()
+
+
+
 
 
