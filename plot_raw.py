787355464a21f56b9cec65eac6c7faba87fbf0c7
Kanaljen <22039515+kanaljen@users.noreply.github.com>
2019-03-07 12:38:11 +0100

    plotraw.py finished

---
+++ plot_raw.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from importdata import data
+
+def plotPair(number):
+    dataset = data()
+    subplot = 210
+    plt.figure(num=None, figsize=(5, 8), dpi=80, facecolor='w', edgecolor='k')
+    for key in dataset:
+        subplot += 1
+        plt.subplot(subplot)
+        plt.plot(dataset[key][number])
+        plt.ylim(0,27)
+        plt.title('{} raw signal'.format(key))
+        plt.ylabel('acceleration')
+        plt.xlabel('samples')
+    plt.subplots_adjust(hspace=0.53)
+    plt.savefig('presentation/figures/rawplot.png')
+
+
+if __name__ == '__main__':
+    plotPair(2)
